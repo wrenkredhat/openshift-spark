386077ba5a36b582c9b17ae3db5a8e77da3828e1
Wolfgang Renk <wrenk@redhat.com>
2019-07-16 15:32:33 +0200

    + group

--- startsiab.sh
+++ startsiab.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-export SIABPWD="xOx"
-
-if [ -z $SIABPWD ]
-then
-  echo "***WARNING!***"
-  echo "Please create a secret (SIABPWD) to change the default password before using"
-else
-  echo "Updating password"
-  /usr/bin/expect << EOD
-timeout 5
-spawn passwd
-expect "assword:"
-send "developer\r"
-expect "New password:"
-send "$SIABPWD\r"
-expect "Retype new password:"
-send "$SIABPWD\r"
-expect eof
-EOD
-
-  unset SIABPWD
-fi
 
 if [ -z $SIABNORMAL ]
 then
@@ -33,7 +11,7 @@ then
 fi
 
 /usr/sbin/shellinaboxd -t -p 8080 --disable-peer-check -d \
-  -s "/:developer:developer:/home/developer:/usr/bin/su developer" -u developer 2>&1
+  -s "/:developer:developer:/home/developer:/usr/bin/su developer" -u developer -g developer 2>&1
 
 echo "---------- ERROR! ----------"
 echo "---------- ERROR! ----------"
