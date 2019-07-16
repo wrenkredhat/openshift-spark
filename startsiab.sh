275f8785fb0ba0ea8f87b3a4a80d2ba43e899a61
Wolfgang Renk <wrenk@redhat.com>
2019-07-16 14:54:48 +0200

    buildurl

--- startsiab.sh
+++ startsiab.sh
@@ -33,7 +33,7 @@ then
 fi
 
 /usr/sbin/shellinaboxd -t -p 8080 --disable-peer-check -d \
-  -s "/:developer:developer:/home/developer:/usr/bin/su developer" -u developer -g developer 2>&1
+  -s "/:developer:developer:/home/developer:/usr/bin/su developer" -u developer 2>&1
 
 echo "---------- ERROR! ----------"
 echo "---------- ERROR! ----------"
