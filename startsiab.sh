ff19428faea61936413c7630522471faa7a843a2
Wolfgang Renk <wrenk@redhat.com>
2019-07-16 16:45:43 +0200

    try BD

--- startsiab.sh
+++ startsiab.sh
@@ -11,7 +11,7 @@ then
 fi
 
 /usr/sbin/shellinaboxd -t -p 8080 --disable-peer-check -d \
-  -s "/:developer:developer:/home/developer:/usr/bin/su developer" -u developer -g developer 2>&1
+    -s "/:developer:developer:/home/developer:/bin/bash" 2>&1  
 
 echo "---------- ERROR! ----------"
 echo "---------- ERROR! ----------"
